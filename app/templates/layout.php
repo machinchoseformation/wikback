f59dd7e8a62008f89fccdd2b648bf59543873e6d
machinchoseformation <machinchoseformation@gmail.com>
2015-08-19 09:30:28 +0200

    add basic styles

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -5,11 +5,14 @@
 	<title><?= $this->e($title) ?></title>
 
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/reset.css') ?>">
+	<link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Roboto:400,300,700'>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
 </head>
 <body>
 	<div class="container">
 		<header>
-			<h1>W :: <?= $this->e($title) ?></h1>
+			<h1>Wikébec Admin :: <?= $this->e($title) ?></h1>
 		</header>
 
 		<section>
--- app/templates/term/show_all_terms.php
+++ app/templates/term/show_all_terms.php
@@ -4,10 +4,18 @@
 	<h2>Tous les termes.</h2>
 	
 	<table>
+		<tr>
+			<th>Id</th>
+			<th>Terme</th>
+			<th>Modification</th>
+			<th>Actions</th>
+		</tr>
 	<?php foreach($terms as $term): ?>
 		<tr>
+			<td><?php echo $term['id'] ?></td>
 			<td><?php echo $term['name'] ?></td>
 			<td><?php echo $term['modifiedDate'] ?></td>
+			<td></td>
 		</tr>
 	<?php endforeach; ?>
 	</table>
